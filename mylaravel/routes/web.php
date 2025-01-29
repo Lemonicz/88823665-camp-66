0f552f1bfb732ffc413bd35344c117a5bf0af261
Lemonicz <66160345@go.buu.ac.th>
2025-01-29 14:18:53 +0700

    test

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -2,18 +2,30 @@
 
 use App\Http\Controllers\MyController;
 use Illuminate\Support\Facades\Route;
-Route::get('/mycontroller',
-    [MyController::class, 'Myfunction']);
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HomeControllerr;
+use App\Http\Controllers\RegisterController;
+use Illuminate\Auth\Events\Login;
 
-Route::post('/mycontroller',
-    [MyController::class, 'Myfunction']);
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login',
+    [LoginController::class, 'index']);
+Route::get('/Register',
+    [RegisterController::class, 'index']);
+/* Route::post('/register',
+    [RegisterController::class, 'index']); */
+Route::get('/Home',
+    [HomeController::class, 'index']);
+    Route::get('/',
+    [HomeController::class, 'index']);
 
-Route::get('/hello/id?',
-function ($val=""){
-    return "<h1>Hello world !!! $val</h1>";
-});
+Route::get('/404',
+    function(){
+    abort(404);
+    });
+Route::get('/505',
+    function(){
+    abort(505);
+    });
 
 ?>
